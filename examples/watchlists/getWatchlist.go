d442f7fbcd713e356942dca1e82799f1993746e3
Michal Tomaszewski <michal_tomaszewski@apple.com>
2020-12-29 09:06:56 -0700

    Add an example for getting watchlists

---
+++ examples/watchlists/getWatchlist.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/tomichal/go-tdameritrade"
+	"golang.org/x/oauth2"
+	"log"
+	"os"
+)
+
+func main() {
+	token := os.Getenv("TDAMERITRADE_CLIENT_ID")
+	if token == "" {
+		log.Fatal("Unauthorized: No token present")
+	}
+	refreshToken := os.Getenv("TDAMERITRADE_REFRESH_TOKEN")
+	if refreshToken == "" {
+		log.Fatal("Unauthorized: No refresh token present")
+	}
+	accountId := os.Getenv("TDAMERITRADE_ACCOUNT_ID")
+	if accountId == "" {
+		log.Fatal("No account ID present")
+	}
+
+	conf := oauth2.Config{
+		ClientID: token,
+		Endpoint: oauth2.Endpoint{
+			TokenURL: "https://api.tdameritrade.com/v1/oauth2/token",
+		},
+		RedirectURL: "https://localhost",
+	}
+
+	tkn := &oauth2.Token{
+		RefreshToken: refreshToken,
+	}
+
+	ctx := context.Background()
+	tc := conf.Client(ctx, tkn)
+
+	c, err := tdameritrade.NewClient(tc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	watchlists, _, error := c.Watchlist.GetAllWatchlistsForAccount(ctx, accountId)
+	if error != nil {
+		log.Fatal(error)
+	}
+	fmt.Println(watchlists)
+}
